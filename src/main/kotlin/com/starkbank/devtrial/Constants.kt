d3d1c43a5578bf410d7bca18c60ea2cbd3c8e314
Caio Werneck <caiosw@gmail.com>
2024-10-05 18:48:18 -0300

    Created Constants object.

---
+++ src/main/kotlin/com/starkbank/devtrial/Constants.kt
@@ -0,0 +1,11 @@
+package com.starkbank.devtrial
+
+object Constants {
+    const val MIN_INVOICE_QUANTITY = 1
+    const val MAX_INVOICE_QUANTITY = 1
+    const val MIN_INVOICE_AMOUNT = 0
+    const val MAX_INVOICE_AMOUNT = 1000000
+    const val PROJECT_ID = "4905314121940992"
+    const val ENV = "sandbox"
+    val INVOICE_TAG_LIST = listOf("manual", "test", "api")
+}
